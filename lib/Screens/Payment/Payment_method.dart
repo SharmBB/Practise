37f5b60b58ef41599504fd62fa84939f9439d469
Sharmilan Sureshwaran <ssharmilan15@gmail.com>
2022-12-06 17:06:57 +0530

    payments updated

--- lib/Screens/Payment/Payment_method.dart
+++ lib/Screens/Payment/Payment_method.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:practise/Screens/CardPayment/cardPayement.dart';
+import 'package:practise/Screens/CashPayment/cashPayment.dart';
 
 class GridDashboard extends StatelessWidget {
   Items item1 = new Items(title: "Cash", img: "assets/png/cash.jpg");
@@ -8,22 +10,12 @@ class GridDashboard extends StatelessWidget {
     title: "Card Patyment",
     img: "assets/png/card.png",
   );
-  Items item3 = new Items(
-    title: "PayPal",
-    img: "assets/png/paypal.png",
-  );
-  Items item4 = new Items(
-    title: "Lanka QR",
-    img: "assets/png/lankaqr.png",
-  );
 
   @override
   Widget build(BuildContext context) {
     List<Items> myList = [
       item1,
       item2,
-      item3,
-      item4,
     ];
     var color = 0xff453658;
     return Flexible(
@@ -34,55 +26,47 @@ class GridDashboard extends StatelessWidget {
         crossAxisSpacing: 18,
         mainAxisSpacing: 15,
         children: myList.map((data) {
-          return Container(
-            decoration: BoxDecoration(
-              color: Color(color),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Image.asset(
-                  data.img,
-                  width: 120,
-                  height: 120,
+          return InkWell(
+              onTap: () {
+                if (data.title == 'Cash') {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CashPayment()),
+                  );
+                } else {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CardPayment()),
+                  );
+                }
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color(color),
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                SizedBox(height: 10),
-                Text(
-                  data.title,
-                  style: GoogleFonts.openSans(
-                    textStyle: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      data.img,
+                      width: 120,
+                      height: 120,
                     ),
-                  ),
+                    SizedBox(height: 10),
+                    Text(
+                      data.title,
+                      style: GoogleFonts.openSans(
+                        textStyle: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-
-                // Text(
-                //   data.subtitle,
-                //   style: GoogleFonts.openSans(
-                //     textStyle: TextStyle(
-                //       color: Colors.white38,
-                //       fontSize: 10,
-                //       fontWeight: FontWeight.w600,
-                //     ),
-                //   ),
-                // ),
-
-                // Text(
-                //   data.event,
-                //   style: GoogleFonts.openSans(
-                //     textStyle: TextStyle(
-                //       color: Colors.white70,
-                //       fontSize: 11,
-                //       fontWeight: FontWeight.w600,
-                //     ),
-                //   ),
-                // ),
-              ],
-            ),
-          );
+              ));
         }).toList(),
       ),
     );
